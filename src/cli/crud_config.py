d0d89b243faaca44af411f89dff4b498c44e2d26
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-12-11 18:05:31 -0300

    fix: update options in config

--- src/cli/crud_config.py
+++ src/cli/crud_config.py
@@ -1,53 +1,49 @@
-"""This file stores values used in `crud_menu`."""
+"""This file stores values used in 'CrudMenu'."""
 
 from beartype.typing import List
 
-MAIN_TITLE: str = "\n[bold yellow]--- CRUD Options ---[/]"
-CREATE_TITLE: str = "\n[bold yellow]--- Menu Create ---[/]"
-DROP_TITLE: str = "\n[bold yellow]--- Drop Create ---[/]"
-
-INVALID_OPTION: str = "[bold red]--- Invalid option: {option} ---[/]"
-DATABASE_CREATED: str = "[bold green]--- Database {database} created ---[/]"
-TABLES_CREATED: str = "[bold green]--- Pre-defined tables created ---[/]"
-END_OF_PROGRAM: str = "[bold green]--- Program has stopped ---[/]"
-START_OF_PROGRAM: str = "[bold green]--- Program has started ---[/]"
-INVALID_OPETATION: str = "[bold red]--- Invalid operation. Check logs. ---[/]"
-
 MAIN_OPTIONS: List[str] = [
-    "1. Add",
-    "2. Read",
-    "3. Update",
-    "4. Delete",
-    "5. Clean",
-    "6. Exit",
+    "CREATE",
+    "READ",
+    "UPDATE",
+    "DELETE",
+    "ANALYZE",
+    "EXIT",
 ]
 
 OPTIONS_CREATE: List[str] = [
-    "1. Create local database",
-    "2. Create pre-defined tables",
-    "3. Create new table",
-    "4. Insert pre-defined lines",
-    "5. Insert new line",
-    "6. Return to CRUD Options",
+    "Create pre-defined database",
+    "Create pre-defined tables",
+    "Create new tables",
+    "Insert pre-defined lines",
+    "Insert new line",
+    "Add new column",
+    "Return to CRUD Options",
 ]
 
 OPTIONS_READ: List[str] = [
-    "1. Show entire table",
-    "2. Show entire column",
-    "3. Show entire row",
-    "4. Return to CRUD Options",
+    "Show entire table",
+    "Show entire column",
+    "Show entire row",
+    "Return to CRUD Options",
 ]
 
 OPTIONS_UPDATE: List[str] = [
-    "1. Alter column",
-    "2. Return to CRUD Options",
+    "Alter column",
+    "Return to CRUD Options",
 ]
 
 OPTIONS_DELETE: List[str] = [
-    "1. Drop database",
-    "2. Drop pre-defined tables",
-    "3. Drop other tables",
-    "4. Delete lines",
-    "5. Delete columns",
-    "6. Return to CRUD Options",
+    "Drop pre-defined database",
+    "Drop pre-defined tables",
+    "Drop other tables",
+    "Delete pre-defined lines",
+    "Delete lines",
+    "Delete columns",
+    "Return to CRUD Options",
+]
+
+OPTIONS_ANALYSIS: List[str] = [
+    "Drop pre-defined database",
+    "Return to CRUD Options",
 ]
